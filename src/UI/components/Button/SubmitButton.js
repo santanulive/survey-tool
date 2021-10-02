538fb48ce519d6769d3f833d37666af08e72d41d
Santanu Dhar <santanudhar1491@gmail.com>
2021-10-02 23:34:54 +0530

    QuestionList component added

--- src/UI/components/Button/SubmitButton.js
+++ src/UI/components/Button/SubmitButton.js
@@ -1,10 +1,16 @@
 import "./css/Button.css";
 import "./css/SubmitButton.css";
 
-const SubmitButton=(props)=>{
-    return(
-        <button type="submit" className="button submit-button" >{props.buttonText}</button>
-    )
-}
+const SubmitButton = (props) => {
+  return (
+    <button
+      type="submit"
+      className="button submit-button"
+      onClick={props.onClick}
+    >
+      {props.buttonText}
+    </button>
+  );
+};
 
 export default SubmitButton;
